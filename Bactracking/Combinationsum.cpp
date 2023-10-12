7c693610b379d943d50181fed15ab74b9bdec56b
Vishal Sharma <vishalsharma15122003@gmail.com>
2023-10-12 16:15:57 +0530

    Update Combinationsum.cpp

--- Bactracking/Combinationsum.cpp
+++ Bactracking/Combinationsum.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 void display(int arr[],int n){
     for(int i=0;i<n;i++){
@@ -6,21 +7,34 @@ void display(int arr[],int n){
     }
     cout<<endl;
 }
-int backtracksum(int arr[],int n,int k){
-    int currsum=0;
-    vector<int> temp;
-    for(int i=0;i<n;i++){
-        currsum+=arr[i];
-        if(currsum==k){
-            temp.push_back(arr[i]);
+void solve(vector<int>& candidates, int target,vector<vector<int>>& ans,vector<int>& output,int i){
+    if(i == candidates.size()){
+        if(target==0){
+            ans.push_back(output);
         }
+            return ;
     }
+
+        if(candidates[i]<=target){
+            output.push_back(candidates[i]);
+            solve(candidates,target-candidates[i],ans,output,i);
+            output.pop_back();
+        }
+
+        solve(candidates,target,ans,output,i+=1);
 }
+vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> output;
+        vector<vector<int>> ans;
+        int ind=0;
+        solve(candidates,target,ans,output,ind);
+        return ans;
+    }
 int main()
 {
     int arr[4]={2,3,6,7};
     int k=7;
     int n=4;
-    display(arr,n);
+    combinationSum(arr,k);
     return 0;
 }
